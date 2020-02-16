b664b53c4bd34ada9c0170157799bebe458955b8
Mafrans <malte.kluft@gmail.com>
2020-02-16 22:53:39 +0100

    add default values

--- src/components/ProgressCard.vue
+++ src/components/ProgressCard.vue
@@ -2,19 +2,40 @@
     <v-card>
         <v-list-item two-line>
             <v-list-item-content>
-                <v-list-item-title class="headline mb-1">Project Name</v-list-item-title>
-                <v-list-item-subtitle>Lorem ipsum dolor sit amet</v-list-item-subtitle>
+                <v-list-item-title class="headline mb-1">{{ title }}</v-list-item-title>
+                <v-list-item-subtitle>{{ subtitle }}</v-list-item-subtitle>
             </v-list-item-content>
         </v-list-item>
         <div class="bar">
-            <v-progress-linear rounded :height="8" :value="25">wow</v-progress-linear>
+            <v-progress-linear rounded :height="height" :value="value">{{ label }}</v-progress-linear>
         </div>
     </v-card>
 </template>
 
 <script>
 export default {
-
+    props: {
+        'value': {
+            default: 50,
+            type: Number,
+        },
+        'title': {
+            default: 'Example Project',
+            type: String,
+        },
+        'subtitle': {
+            default: `This is an example progress bar that doesnt really mean anything. But hey, it's something ¯\\_(ツ)_/¯`,
+            type: String,
+        },
+        'label': {
+            default: '',
+            type: String,
+        },
+        'height': {
+            default: 10,
+            type: Number
+        },
+    }
 }
 </script>
 
