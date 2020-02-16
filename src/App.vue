e24eb50b1092f081aa7e7b9d182d0f7558207a90
Mafrans <malte.kluft@gmail.com>
2020-02-16 22:54:16 +0100

    add custom project for testing

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,27 @@
 <template>
-  <v-app>
-      
-  </v-app>
+    <v-app>
+        <progress-card v-for="(bar, i) in bars" :key="i" :title="bar.title" :subtitle="bar.subtitle" :value="bar.value"/>
+    </v-app>
 </template>
 
 <script>
-export default {
-  name: 'App',
+import ProgressCard from './components/ProgressCard'
 
-  data: () => ({
-    //
-  }),
+export default {
+    name: 'App',
+
+    components: {
+        ProgressCard
+    },
+
+    data: () => ({
+        bars: [
+            {
+                title: 'Custom Project',
+                subtitle: `Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam`,
+                value: 30,
+            }
+        ]
+    }),
 };
 </script>
