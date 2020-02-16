e113e11df2eeb25b86c282d82d269cc80d44bc79
Mafrans <malte.kluft@gmail.com>
2020-02-16 22:59:16 +0100

    format documents

--- src/App.vue
+++ src/App.vue
@@ -1,37 +1,37 @@
 <template>
-  <v-app>
-    <v-content>
-      <v-container>
-        <progress-card
-          v-for="(bar, i) in bars"
-          :key="i"
-          :title="bar.title"
-          :subtitle="bar.subtitle"
-          :value="bar.value"
-        />
-      </v-container>
-    </v-content>
-  </v-app>
+    <v-app>
+        <v-content>
+            <v-container>
+                <progress-card
+                    v-for="(bar, i) in bars"
+                    :key="i"
+                    :title="bar.title"
+                    :subtitle="bar.subtitle"
+                    :value="bar.value"
+                />
+            </v-container>
+        </v-content>
+    </v-app>
 </template>
 
 <script>
 import ProgressCard from "./components/ProgressCard";
 
 export default {
-  name: "App",
+    name: "App",
 
-  components: {
-    ProgressCard
-  },
+    components: {
+        ProgressCard,
+    },
 
-  data: () => ({
-    bars: [
-      {
-        title: "Custom Project",
-        subtitle: `Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam`,
-        value: 30
-      }
-    ]
-  })
+    data: () => ({
+        bars: [
+            {
+                title: "Custom Project",
+                subtitle: `Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam`,
+                value: 30,
+            },
+        ],
+    }),
 };
 </script>
--- src/components/ProgressCard.vue
+++ src/components/ProgressCard.vue
@@ -2,12 +2,16 @@
     <v-card>
         <v-list-item two-line>
             <v-list-item-content>
-                <v-list-item-title class="headline mb-1">{{ title }}</v-list-item-title>
+                <v-list-item-title class="headline mb-1">
+                    {{ title }}
+                </v-list-item-title>
                 <v-list-item-subtitle>{{ subtitle }}</v-list-item-subtitle>
             </v-list-item-content>
         </v-list-item>
         <div class="bar">
-            <v-progress-linear rounded :height="height" :value="value">{{ label }}</v-progress-linear>
+            <v-progress-linear rounded :height="height" :value="value">{{
+                label
+            }}</v-progress-linear>
         </div>
     </v-card>
 </template>
@@ -15,28 +19,28 @@
 <script>
 export default {
     props: {
-        'value': {
+        value: {
             default: 50,
             type: Number,
         },
-        'title': {
-            default: 'Example Project',
+        title: {
+            default: "Example Project",
             type: String,
         },
-        'subtitle': {
+        subtitle: {
             default: `This is an example progress bar that doesnt really mean anything. But hey, it's something ¯\\_(ツ)_/¯`,
             type: String,
         },
-        'label': {
-            default: '',
+        label: {
+            default: "",
             type: String,
         },
-        'height': {
+        height: {
             default: 10,
-            type: Number
+            type: Number,
         },
-    }
-}
+    },
+};
 </script>
 
 <style scoped>
